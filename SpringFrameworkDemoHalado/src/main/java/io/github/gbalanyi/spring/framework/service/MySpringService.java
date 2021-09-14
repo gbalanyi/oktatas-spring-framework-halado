8f7346fc4f4eb7b4f11e155c9711aabac4cacd20
Balanyi Gábor <balanyi.g@gmail.com>
2021-09-14 16:30:45 +0200

    11. Annotáció alapú esemény kezelés.

--- SpringFrameworkDemoHalado/src/main/java/io/github/gbalanyi/spring/framework/service/MySpringService.java
+++ SpringFrameworkDemoHalado/src/main/java/io/github/gbalanyi/spring/framework/service/MySpringService.java
@@ -1,11 +1,11 @@
 package io.github.gbalanyi.spring.framework.service;
 
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MySpringService implements ApplicationListener<ContextRefreshedEvent> {
+public class MySpringService {
 
     private final TimestampService timestampService;
 
@@ -13,9 +13,9 @@ public class MySpringService implements ApplicationListener<ContextRefreshedEven
         this.timestampService = timestampService;
     }
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        System.out.println("ApplicationContext refreshed.");
+    @EventListener
+    public void myEventHandler(ContextRefreshedEvent event) {
+        System.out.println(String.format("%d ApplicationContext refreshed.", event.getTimestamp()));
     }
 
     public void greeting(String name) {
